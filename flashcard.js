17384b31c8ba4d10d833c599763c0fca349c863c
loganpowell <loganp@tepper.cmu.edu>
2017-04-20 14:58:02 -0400

    not sure if this one was easy or I read the instructions wrong

---
+++ flashcard.js
@@ -0,0 +1,36 @@
+function BasicCard(front, back){
+    if (!(this instanceof BasicCard))
+        return new BasicCard(front, back)
+    this.front = front
+    this.back = back
+}
+
+var firstPresident = BasicCard(
+    "Who was the first president of the United States?", "George Washington")
+
+console.log(firstPresident.front)
+
+function ClozeCard(text, cloze){
+  if (!(this instanceof ClozeCard)) {
+    return new ClozeCard(text, cloze)
+  }
+  this.fullText = text
+  this.cloze = cloze
+  var matcher = new RegExp(this.cloze)
+  if (!this.fullText.match(matcher)) {
+    console.error("shit's broke!");
+  }
+  var cutoff = this.cloze.length
+  this.partial = '...' + this.fullText.slice(cutoff)
+}
+
+var firstPresidentCloze = ClozeCard(
+    "George Washington was the first president of the United States.", "George Washington");
+
+console.log("firstPresidentCloze.fullText: " + firstPresidentCloze.fullText);
+console.log("firstPresidentCloze.partial: " + firstPresidentCloze.partial);
+
+var brokenCloze = ClozeCard(
+    "George Washington was the first president of the United States.", "poop string");
+
+console.log("brokenCloze.fullText: " + brokenCloze.fullText);
